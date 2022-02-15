386341772edf7186097fda24c457d5fa4a626a07
KreizerHgz <37186178+KreizerHgz@users.noreply.github.com>
2022-02-15 18:37:39 +0100

    Basic SQL setup

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'musict'
+});
 
 app.get("/", (req, res) => {
     res.send("Hello");
